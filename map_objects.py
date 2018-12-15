e93a4f5b12a76a4f3a4030612577199eb82e1f67
Michael McCabe <mccabmic@oregonstate.edu>
2018-12-15 12:35:59 -0600

    Add functunality for FOV

--- map_objects.py
+++ map_objects.py
@@ -9,6 +9,7 @@ class Tile:
             block_sight = blocked
         
         self.block_sight = block_sight
+        self.explored = False
 
 class Rect:
     """
--- render_functions.py
+++ render_functions.py
@@ -1,21 +1,35 @@
 import tcod as libtcod
 
 # Go through all entities and call putEntity
-def render_all(con, entities, game_map, screen_width, screen_height, colors):
+def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
+    if fov_recompute:
+        # Draw the map
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                # use libcod fov_map to determine visibility
+                visible = libtcod.map_is_in_fov(fov_map, x, y)
 
-    # Draw the map
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            wall = game_map.tiles[x][y].block_sight
-            
-            if wall:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
-            else:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
-    
+                # a wall is something that will block_sight
+                wall = game_map.tiles[x][y].block_sight
+
+                #if the player can see the wall render it light it up and mark it as explored
+                if visible:
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, colors.get('light_wall'), libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
+                    game_map.tiles[x][y].explored = True
+                # otherwise don't draw it unless the player already saw it
+                
+                elif game_map.tiles[x][y].explored:
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+        
     # Draw the entities
     for entity in entities:
-        draw_entity(con, entity)
+        draw_entity(con, entity, fov_map)
     
     #Remember, you're drawing these but you need to blit the screen
     libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
@@ -26,9 +40,10 @@ def clear_all(con, entities):
         clear_entity(con, entity)
 
 # Put a single entity onto the screen
-def draw_entity(con, entity):
-    libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+def draw_entity(con, entity, fov_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        libtcod.console_set_default_foreground(con, entity.color)
+        libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
 # Clear a single entity from the screen
 def clear_entity(con, entity):
